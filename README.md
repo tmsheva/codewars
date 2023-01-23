5c9043a753ae5b42c43850a968f7e8efc8889abc
Артём Шевченко <artemshevchenko@MacBook-Air-Artem.local>
2023-01-23 14:34:07 +0300

    even_or_odd

---
+++ README.md
@@ -0,0 +1,2 @@
+### Repository with my options for solving problems with codewars.
+### Репозиторий с моими вариантами решений заданий с codewars.
---
+++ even_or_odd.js
@@ -0,0 +1,9 @@
+/**
+ * Проверяет чётное число или нечётное.
+ *
+ * @param {number} number Число для проверки. Even - чётное, Odd - нечётное.
+ *
+ */
+function evenOrOdd(number) {
+  return number % 2 == 0 ? "Even" : "Odd";
+}
